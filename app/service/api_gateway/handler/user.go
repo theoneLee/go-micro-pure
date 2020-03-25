97c7e7ba85f8e28561312d0d948db613072114ef
theoneLee <623507020@qq.com>
2020-03-25 14:45:15 +0800

    准备不使用go micro broker，引入自定义的nats作为mq。不使用common存放model代码。代码是写的少，但是实际上职责就耦合了。因此，没专门团队去做，微服务还是增加很多复杂度。。 todo： - 断路器 - 链路追踪

--- app/service/api_gateway/handler/user.go
+++ app/service/api_gateway/handler/user.go
@@ -20,6 +20,7 @@ func SaveUser(c *gin.Context) {
 			"code": 0,
 			"data": gin.H{
 				"user": resp.GetUser(), //todo 这里是直接使用proto定义的model的json tag。而这个tag是无法直接在proto文件处理的。因此，要自定义返回给前端的json内容，可以自己在这里打包,更甚的，可以在定义个wrapper，然后手动将proto定义的打包成wrapper
+				//todo 因为网关层可能会调用多个服务，然后组合数据，因此该wrapper的model定义和微服务里面的可能不一致。因此完全分开也好，其实放到common的话就是为了网关和微服务共用而已。
 				//"boolVal":resp.GetUser().Id,
 			},
 		})
